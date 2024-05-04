3c83f329f6179788b6976642592b26f77646d663
Muhammad Qamar Nisar <69616044+qamar-nisar@users.noreply.github.com>
2024-05-04 20:09:01 +0000

    variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 2312
+let accountEmail = "abc@google.com"
+var accountPassword = "213123213"
+accountCity = "Tokyo"
+let accountState
+
+// accountId = 23213123
+// console.log(accountId)
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
