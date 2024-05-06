e34896dccde62fb4a58da29f958ea4345239ae7e
Muhammad Qamar Nisar <69616044+qamar-nisar@users.noreply.github.com>
2024-05-06 11:21:55 +0000

    comparision

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,15 @@
+console.log("2 > 1 " + (2 > 1));                       // true
+console.log("2 >= 1 " + (2 >= 1));                     // true
+console.log("2 < 1 " + (2 < 1));                       // false
+console.log("2 <= 1 " + (2 <= 1));                     // false
+console.log("2 == 1 " + (2 == 1));                     // false
+console.log("2 != 1 " + (2 != 1));                     // true
+console.log("\"2\" > 1 " + ("2" > 1));                 // true
+console.log("\"02\" > 1 " + ("02" > 1));               // true
+console.log("null > 0 " + (null > 0));                 // false
+console.log("null == 0 " + (null == 0));               // false
+console.log("null >= 0 " + (null >= 0));               // true
+console.log("undefined == 0 " + (undefined == 0));     // false
+console.log("undefined > 0 " + (undefined > 0));       // false
+console.log("undefined >= 0 " + (undefined >= 0));     // false
+console.log("undefined < 0 " + (undefined < 0));       // false
